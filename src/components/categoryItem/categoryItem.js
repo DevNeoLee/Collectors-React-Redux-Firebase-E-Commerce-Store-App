54e8710f1a3db4ccb8480dcc34e08f85374e61c2
DevNeoLee <devneolee@gmail.com>
2021-01-14 15:01:02 -0800

    Implement item data details

--- src/components/categoryItem/categoryItem.js
+++ src/components/categoryItem/categoryItem.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 import categoryItemStyle from './categoryItem.module.scss'
 
-const CategoryItem = ({ name}) => {
-
+const CategoryItem = ( { name, imageUrl, price} ) => {
+    console.log(imageUrl)
     return (
         <div className={categoryItemStyle.id}>
-            <h3 className={categoryItemStyle.title}> {name.toUpperCase()}</h3>
-            {/* <img src={require('/media-images/animation/cowboy.jpg')} /> */}
-             {/* <img src={require('/logo.png')} /> */}
-            {/* <img src="./media-images/animation/batman.jpg" alt="" width="100px" height="100"/> */}
+            <h3 className={categoryItemStyle.title}> { name.toUpperCase() }</h3>
+            <img src={imageUrl} alt="" width="200" />
+            <p>Price: $ { price }</p>
         </div>
     )
 }
