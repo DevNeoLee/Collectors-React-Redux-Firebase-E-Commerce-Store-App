cfd2ab1369d3bfe6bff39c6c2010710fc93824d2
DevNeoLee <devneolee@gmail.com>
2021-01-16 09:43:20 -0800

    Add login signin components to header and Css

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,13 +5,20 @@ import { Link } from 'react-router-dom';
 import headerStyle from './header.module.scss'
 import logo from '../../logo.png'
 
-const Header = ({ }) => (
+import { auth } from '../../firebase/utils'
+
+const Header = ({ currentUser }) => (
   <div className={headerStyle.header} >
         <div className={headerStyle.logo}><Link to="/"><img src={ logo } alt="logo" width={200}/></Link></div>
         <div className={headerStyle.links}>
-            <div><Link to="/shop">Shop</Link></div>
-            <div><Link to="/">About</Link></div>
+            <div className={headerStyle.link}><Link to="/shop">Shop</Link></div>
+            <div className={headerStyle.link}><Link to="/">About</Link></div>
         </div>
+        { currentUser ? 
+          <div onClick={() => auth.signOut() } className={headerStyle.link}>Log out</div>
+          : 
+          <Link to="/login"><div className={headerStyle.link}>Log in</div></Link>
+        }
   </div>
 );
 
