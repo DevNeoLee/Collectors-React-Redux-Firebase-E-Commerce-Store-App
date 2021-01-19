0e8ae66051d6aad9765f482cdd789684313dc2c2
DevNeoLee <devneolee@gmail.com>
2021-01-19 01:03:07 -0800

    Add total product count on cart-header by putting selecting reduced number of cart products

--- src/components/cart-header/cart-header.js
+++ src/components/cart-header/cart-header.js
@@ -6,11 +6,11 @@ import { toggleCartHidden } from '../../redux/cart/cart-actions'
 import cartHeaderStyle from './cart-header.module.scss'
 import { GiShoppingCart } from 'react-icons/gi'
 
-const CartHeader = ({ toggleHidden }) => {
+const CartHeader = ({ toggleHidden, productCount }) => {
 
     return (
         <div className={ cartHeaderStyle.container} onClick={toggleHidden}>
-            <span className={ cartHeaderStyle.count}>0</span>
+            <span className={ cartHeaderStyle.count}>{productCount}</span>
             <div className={ cartHeaderStyle.icon }><GiShoppingCart size={40}/></div>
         </div>
     )
@@ -20,5 +20,9 @@ const mapDispatchToProps = dispatch => ({
     toggleHidden: () => dispatch(toggleCartHidden())
  })
 
-export default connect(null, mapDispatchToProps)(CartHeader)
+const mapStateToProps = ({ cart: { cartProducts }})=> ({
+    productCount: cartProducts.reduce((countAccumlator, product) => countAccumlator + product.quantity, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartHeader)
 
