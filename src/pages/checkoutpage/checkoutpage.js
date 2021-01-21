e3aba7d1f35409d6d8adb67428d0441bb54d5f31
DevNeoLee <devneolee@gmail.com>
2021-01-21 15:39:37 -0800

    Update checkoutpage

--- src/pages/checkoutpage/checkoutpage.js
+++ src/pages/checkoutpage/checkoutpage.js
@@ -21,9 +21,9 @@ const CheckoutPage = ({cartProducts, total})=> {
             )}
             <div>Total: $ {total}</div>
             <div className='test-warning'>
-                *Please use the following test credit card for payments*
+                *Please use the following test credit card for payments
             <br />
-                4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+                4242 4242 4242 4242 - Exp: 01/25 - CVV: 123
             </div>
             <StripeCheckoutButton price={total} />
         </>
