1ee825df2b47e3953bb4783e0ebfcf5b2a3fba5e
DevNeoLee <devneolee@gmail.com>
2021-01-31 19:53:27 -0800

    Add componentDidUpdate for window.scroll up to the top of the page

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,15 +1,26 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import homepageStyles from './homepage.module.scss';
 import Directory from '../../components/directory/directory';
 import Slider from '../../components/slider/slider';
 
-export default function Homepage() {
-    return (
+import { withRouter } from "react-router-dom";
+
+class Homepage extends Component {
+	componentDidUpdate(prevProps) {
+		if (this.props.location !== prevProps.location) {
+			window.scrollTo(0, 0);
+		}
+    }
+    render() {
+          return (
         <div className={homepageStyles.homepage}>
             <Slider />
             <Directory />
 
         </div>
-    )
+    );
+    } 
 }
+
+export default withRouter(Homepage);
