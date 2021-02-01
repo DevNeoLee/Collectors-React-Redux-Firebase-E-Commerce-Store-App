28d04a6df385e8180ef27b2f78474c4d14ef16d5
DevNeoLee <devneolee@gmail.com>
2021-01-31 19:50:50 -0800

    Set Footer CSS

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-import footerStyle from './footer.module.scss'
+import { Link } from 'react-router-dom';
+
+import footerStyle from './footer.module.scss';
+import logo from '../../logoMain.png'
 
 const footer = () => {
 
     return (
         <div className={footerStyle.container}>
-            footer
+          <div>
+            <a href=""></a>       
+            <Link to="/"><img src={ logo } alt="logo" width={400}/></Link>
+            <small>&copy; 2020</small>
+          </div>
+          <div>
+            <h5>Features</h5>
+            <ul>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+          </div>
+          <div>
+            <h5>Resources</h5>
+            <ul>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+          </div>
+          <div>
+            <h5>About</h5>
+            <ul>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+          </div>
         </div>
-    )
+    );
 }
 
-export default footer
+export default footer;
 
