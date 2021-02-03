a7d203aac2edbb8e63a583ce1770173d30d0f6be
DevNeoLee <devneolee@gmail.com>
2021-02-03 14:46:05 -0800

    Create collections component mapping collection data from redux

---
+++ src/components/collections/collections.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import Category from '../category/category';
+
+import { selectCollections } from '../../redux/shop/shop-selectors';
+
+import collectionsStyle from './collections.module.scss';
+
+const Collections = ({ collections }) => (
+  <div className={ collectionsStyle.main }>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <Category key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(Collections);
--- src/pages/shoppage/shoppage.js
+++ src/pages/shoppage/shoppage.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import Category from '../../components/category/category';
-
-import { selectCollections } from '../../redux/shop/shop-selectors';
+import Collections from '../../components/collections/collections'
 
 import shoppageStyle from './shoppage.module.scss';
 
-const Shoppage = ({ collections }) => {
+const Shoppage = () => {
         return (
-
-            <div className={shoppageStyle.container}>
-                { collections.map(({id, ...otherItems})=>(
-                    <Category key={id} {...otherItems} />
-                ))}
-            </div>
+           <Collections />
         )
-
 }
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(Shoppage);
+export default Shoppage;
