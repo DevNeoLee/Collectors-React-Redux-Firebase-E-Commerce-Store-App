74e33ffc29eac5cdf56f231daed1b8290b40edb3
DevNeoLee <devneolee@gmail.com>
2021-02-03 11:29:27 -0800

    Create categorypage

--- src/pages/categorypage/categorypage.js
+++ src/pages/categorypage/categorypage.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import CategorypageStyles from './categorypage.module.scss';
 
+import CategoryItem from '../../components/categoryItem/categoryItem';
+
 export default function Categorypage() {
     return (
         <div className={CategorypageStyles.Categorypage}>
-      
+            <CategoryItem />
         </div>
     )
 }
