97f3d8691bab78b356099f5acb97e76c177e48c7
DevNeoLee <devneolee@gmail.com>
2021-02-03 13:48:55 -0800

    Transfer shop data, state to redux with shop-reducer and re-connect them in shoppage

--- src/pages/shoppage/shoppage.js
+++ src/pages/shoppage/shoppage.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data.js';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import Category from '../../components/category/category';
 
+import { selectCollections } from '../../redux/shop/shop-selectors';
+
 import shoppageStyle from './shoppage.module.scss';
 
-export default class ShopPage extends Component {
-    constructor() {
-        super()
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const collections = this.state.collections
-        
+const Shoppage = ({ collections }) => {
         return (
 
             <div className={shoppageStyle.container}>
@@ -24,5 +17,11 @@ export default class ShopPage extends Component {
                 ))}
             </div>
         )
-    }
+
 }
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+
+export default connect(mapStateToProps)(Shoppage);
