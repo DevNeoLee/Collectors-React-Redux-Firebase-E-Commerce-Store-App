38e45638739bf5afc9a578e8c70c0797a4e6d787
DevNeoLee <devneolee@gmail.com>
2021-02-04 17:45:31 -0800

    Update onMouseclick to link to the checkout page, onMouseEnter to hover dropdown component

--- src/components/cart-header/cart-header.js
+++ src/components/cart-header/cart-header.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import backgroundImage from '../../icon.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import backgroundImage from '../../icon.png';
 
-import { toggleCartHidden } from '../../redux/cart/cart-actions'
-import { selectCartProductsCount } from '../../redux/cart/cart-selectors'
+import { toggleCartHidden } from '../../redux/cart/cart-actions';
+import { selectCartProductsCount } from '../../redux/cart/cart-selectors';
 
-import cartHeaderStyle from './cart-header.module.scss'
-import { GiShoppingCart } from 'react-icons/gi'
+import cartHeaderStyle from './cart-header.module.scss';
+import { GiShoppingCart } from 'react-icons/gi';
 
 const CartHeader = ({ toggleHidden, productCount }) => {
 
     return (
-            <div className={ cartHeaderStyle.container} onClick={toggleHidden}>
-                <div className={ cartHeaderStyle.icon }><GiShoppingCart size={30}/></div>
-                <div className={ cartHeaderStyle.countContainer}>
-                    <img className={ cartHeaderStyle.image } src={backgroundImage}/>
-                    <div className={ cartHeaderStyle.number}>{productCount}</div>
+            <Link to="/checkout">
+                <div className={ cartHeaderStyle.container} onMouseEnter={toggleHidden}>
+                    <div className={ cartHeaderStyle.icon }><GiShoppingCart size={30}/></div>
+                    <div className={ cartHeaderStyle.countContainer}>
+                        <img className={ cartHeaderStyle.image } src={backgroundImage}/>
+                        <div className={ cartHeaderStyle.number}>{productCount}</div>
+                    </div>
+                    <p className={ cartHeaderStyle.cart}>Cart</p>
                 </div>
-                <p className={ cartHeaderStyle.cart}>Cart</p>
-            </div>
+            </Link>
     )
-}
+};
 
 const mapDispatchToProps = dispatch => ({
     toggleHidden: () => dispatch(toggleCartHidden())
- })
+ });
 
 const mapStateToProps = createStructuredSelector({
     productCount: selectCartProductsCount
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartHeader)
+export default connect(mapStateToProps, mapDispatchToProps)(CartHeader);
 
